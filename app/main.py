f703e5bd7bae6fc72b6ab872938f6ae10b0e78ad
k6zma <87338187+k6zma@users.noreply.github.com>
2024-05-15 15:49:08 +0300

    refubbed logic in tournament simulation

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from src.data_loader import DataLoader
 from src.tournament import Tournament
 from src.generate_synthetic_data import TournamentMatrix
 from src.generating_image_tt import TournamentTableGenerator
+from itertools import permutations
+from math import factorial
 
 import pandas as pd
 import random
 from tqdm import tqdm
-from itertools import permutations
 
 def random_distribution(players, total_positions):
     all_positions = list(range(total_positions))
@@ -17,83 +18,58 @@ def random_distribution(players, total_positions):
     remaining_players = players[random_count:]
     return distributed_players, remaining_players
 
-def calculate_win_probability(player_name, position, data):
+def calculate_win_probabilities(data):
     players = data.columns
-    position_counts = 0
-    total_simulations = 0
-    
+    total_permutations = factorial(len(players))
+    win_counts = {player: 0 for player in players}
+
     for perm in permutations(players):
         simulate_order = list(perm)
-        simulate_order[position] = player_name
         tournament = Tournament(data)
         _, winner = tournament.simulate_tournament(simulate_order)
-        
-        if winner == player_name:
-            position_counts += 1
-        total_simulations += 1
-        
-    probability = position_counts / total_simulations if total_simulations > 0 else 0
-    return probability
+        win_counts[winner] += 1
 
-def normalize_probabilities(player_probabilities):
-    total_probability = sum(player_probabilities.values())
-    for player in player_probabilities:
-        player_probabilities[player] /= total_probability
-    return player_probabilities
+    probabilities = {player: win_counts[player] / total_permutations for player in players}
+    return probabilities
 
 def main():
     choice = input("Вы хотите использовать собственные данные? (да/нет): ").strip().lower()
     if choice == 'да':
         path = input("Введите путь до файла: ").strip()
         data = DataLoader.load_data(path)
-        print(data)
     else:
         print("Генерируем данные...")
         tm = TournamentMatrix()
         data = tm.get_matrix_df()
-        print(data)
 
+    print(data)
     tournament = Tournament(data)
     heroes = tournament.collect_heroes()
     total_positions = len(heroes)
-
     distributed_players, remaining_players = random_distribution(heroes, total_positions)
 
     print("Случайно распределенные игроки по позициям:")
     for position, player in distributed_players.items():
         print(f"Позиция {position + 1}: {player}")
 
-    optimal_positions = {}
     print("\nПоиск оптимальных позиций для оставшихся игроков:")
     for player in tqdm(remaining_players, desc="Обработка игроков"):
         best_position, reason = tournament.find_best_position_for_player(player, distributed_players)
         distributed_players[best_position] = player
-        optimal_positions[player] = (best_position, reason)
 
-    print("\nИгроки, для которых выбраны оптимальные места:")
-    for player, (position, reason) in optimal_positions.items():
-        print(f"Игрок: {player}, Оптимальная позиция: {position + 1}, Причина: {reason}")
-
-    player_probabilities = {}
-    for position in sorted(distributed_players.keys()):
-        player = distributed_players[position]
-        probability = calculate_win_probability(player, position, data)
-        player_probabilities[player] = probability
-
-    normalized_probabilities = normalize_probabilities(player_probabilities)
+    probabilities = calculate_win_probabilities(data)
 
     print("\nИтоговое распределение игроков по позициям:")
-    for player, probability in normalized_probabilities.items():
+    for player, probability in probabilities.items():
         print(f"Игрок {player} (Вероятность победы: {probability:.2%})")
 
     final_players_order = [distributed_players[i] for i in range(total_positions)]
-
     tournament_details, winner = tournament.simulate_tournament(final_players_order)
-
     for round_details in tournament_details:
         print(f"\nРаунд {round_details[0]['round']}:")
         for match in round_details:
             print(f"  {match['player1']} против {match['player2']} -> Победитель: {match['winner']}")
+
     print(f"\nПобедитель турнира: {winner}")
 
     generator = TournamentTableGenerator(tournament_details, winner)
